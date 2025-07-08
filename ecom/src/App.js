1d2603f9d9711c4c7c1788be91cbdd6729a7c666
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-08 20:05:06 +0530

    animated border added

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -19,6 +19,18 @@ function App() {
           <label className='browse'>
             <input placeholder='browse by...' className='inside-browse'/>
           </label>
+          <div className='categories'>
+            <h1>Women's</h1>
+            <div className='sub-categories'>
+              <ul className='item-container'>
+                <li className='cat-item-holder'><a className='category-item'href="">Category-2</a></li>
+                <li className='cat-item-holder'><a className='category-item' href="">Category-3</a></li>
+                <li className='cat-item-holder'><a className='category-item' href="">Category-4</a></li>
+                <li className='cat-item-holder'><a className='category-item'href="">Category-5</a></li>
+                <li className='cat-item-holder'><a className='category-item' href="">Category-1</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
      </div>
