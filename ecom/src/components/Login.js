8233065c15a15e670ce52b2fd8463a2d164d0c29
somelamedude-git <cozydeveloper843@gmail.com>
2025-07-13 06:07:31 +0530

    login page update

--- ecom/src/components/Login.js
+++ ecom/src/components/Login.js
@@ -3,10 +3,27 @@ import '../styles/Login.css';
 export default function LoginPage(){
     return(
         <div className='Page'>
-        <div className='login-container'>
-            <img src='/fashion.jpg' className='fashion'/>
-            <div className='foggy-container'>
-
+        <div className='image-holder'>
+            <img src='/fash.png' className='fashion'/>
+        </div>
+        <div className='login-form-container'>
+            <div className='main-form'>
+                <form className='login-form'>
+                    <div className='header'>
+                        <h1>LOGIN</h1>
+                    </div>
+                    <div className='inputs'>
+                        <input className='info-input' placeholder='Username'/>
+                        <input className='info-input' placeholder='Name'/>
+                    </div>
+                    <div className='forgot'>
+                        <a href='#' className='forgot-password'>Forgot Password?</a>
+                    </div>
+                    <div className='line'></div>
+                    <div>
+                        <button type='submit' className='submit-login'>SUBMIT</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
